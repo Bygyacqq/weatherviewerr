0408fb243909b0c85c836d64125b88c71ef5c48e
Dayana Pulido <144375079+Bygyacqq@users.noreply.github.com>
2024-05-22 00:38:47 -0500

    WeatherViewer

--- app/src/main/java/weather.java
+++ app/src/main/java/weather.java
@@ -1,45 +1,34 @@
+import java.sql.Time;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
-
 public class weather
 {
-    public String dayOfWeek;
- public String minTemp;
- public String maxTemp;
- public String humidity;
- public String description;
- public String iconURL;
-    public void Weather(long timeStamp, double minTemp, double maxTemp,
-                        double humidity, String description, String iconName) {
-        // NumberFormat to format double temperatures rounded to integers
+    public final String dayOfweek;
+    public final String minTemp;
+    public final String maxTemp;
+    public final String humidity;
+    public final String description;
+    public final String iconURL;
+
+    public weather(String dayOfweek, double minTemp, double maxTemp, double humidity,
+                   String description, String iconURL, long timeStamp, String iconName) {
         NumberFormat numberFormat = NumberFormat.getInstance();
         numberFormat.setMaximumFractionDigits(0);
-        this.dayOfWeek = convertTimeStampToDay(timeStamp);
+        this.dayOfweek = convertTimeStampToDay(timeStamp);
         this.minTemp = numberFormat.format(minTemp) + "\u00B0F";
         this.maxTemp = numberFormat.format(maxTemp) + "\u00B0F";
-        this.humidity = NumberFormat.getPercentInstance().format(humidity / 100.0);
+        this.humidity = numberFormat.getPercentInstance().format(humidity/100.0);
         this.description = description;
-        this.iconURL =
-                "http://openweathermap.org/img/w/" + iconName + ".png";
+        this.iconURL = "http://openweathermap.org/img/w/" + iconName + ".png";
     }
 
-    public weather(String dayOfWeek, String minTemp, String maxTemp, String humidity, String description, String iconURL) {
-        this.dayOfWeek = dayOfWeek;
-        this.minTemp = minTemp;
-        this.maxTemp = maxTemp;
-        this.humidity = humidity;
-        this.description = description;
-        this.iconURL = iconURL;
-    }
-
-    private static String convertTimeStampToDay(long timeStamp)
-    {
+    private static String convertTimeStampToDay(long timeStamp) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(timeStamp*1000);
-        TimeZone tz = TimeZone.getDefault();
-        calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+        TimeZone timeZone = TimeZone.getDefault();
+        calendar.add(Calendar.MILLISECOND, timeZone.getOffset(calendar.getTimeInMillis()));
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE");
         return dateFormat.format(calendar.getTime());
     }
